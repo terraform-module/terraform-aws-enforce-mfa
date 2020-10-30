e9439edad97e71656d7177945bcf85ff445be665
Vicken Simonian <vicken@papayapay.com>
2020-10-30 14:12:31 -0700

    Deny all statement should specify not_actions
    Fixes #1

--- data.tf
+++ data.tf
@@ -279,7 +279,7 @@ data aws_iam_policy_document this {
     content {
       sid    = "DenyAllExceptListedIfNoMFA"
       effect = "Deny"
-      actions = [
+      not_actions = [
         "iam:CreateVirtualMFADevice",
         "iam:EnableMFADevice",
         "iam:GetUser",
